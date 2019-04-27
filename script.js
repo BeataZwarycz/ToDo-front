d8e81a617803cb85875c576727bdef3639842a31
Agata Mastalska <mastalska.agata@gmail.com>
2019-04-27 17:12:09 +0200

    add script

---
+++ script.js
@@ -0,0 +1,73 @@
+const app = document.getElementById('root');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+const sticker = document.createElement('div');
+container.setAttribute('class', 'sticker');
+
+app.appendChild(container);
+container.appendChild(sticker);
+
+//LOAD ALL CATEGORIES 
+const requestLoadCategory = new XMLHttpRequest();
+requestLoadCategory.open('GET', 'https://to-do-coderscamp.herokuapp.com/api/category/', true);
+requestLoadCategory.onload = function () {
+    var data = JSON.parse(this.response);
+    if (requestLoadCategory.status >= 200 && requestLoadCategory.status < 400) {
+        data.forEach(Categories => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'sticker');
+            Categories.title = Categories.title.substring(0, 300);
+            p.textContent = `${Categories.title}...`;
+            container.appendChild(card);
+        });
+    } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `it's not working!`;
+        app.appendChild(errorMessage);
+    }
+}
+
+requestLoadCategory.send();
+
+//LOAD ALL TASKS FROM CATEGORY
+var requestLoadCategoryTasks = new XMLHttpRequest();
+requestLoadCategoryTasks.open('GET', 'https://to-do-coderscamp.herokuapp.com/api/tasks/:categoryId', true);
+requestLoadCategoryTasks.onload = function () {
+
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    if (requestLoadCategoryTasks.status >= 200 && requestLoadCategoryTasks.status < 400) {
+        data.forEach(Tasks => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'sticker-content');
+
+            Tasks.title = Tasks.title.substring(0, 300);
+            p.textContent = `${Tasks.title}...`;
+            container.appendChild(card);
+        });
+    } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `it's not working!`;
+        app.appendChild(errorMessage);
+    }
+}
+
+requestLoadCategoryTasks.send();
+
+//UPDATE TASK IN DATEBASE
+var requestEditTask = new XMLHttpRequest();
+requestEditTask.open('GET', 'https://to-do-coderscamp.herokuapp.com/api/tasks/:id', true);
+requestEditTask.onload = function () {
+    var data = JSON.parse(this.response);
+    if (requestEditTask.status >= 200 && requestEditTask.status < 400) {
+        //nie mam pojęcia co tu wpisać
+    } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `it's not working!`;
+        app.appendChild(errorMessage);
+    }
+}
+
+requestEditTask.send();
